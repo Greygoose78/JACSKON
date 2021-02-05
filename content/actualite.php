ede12fdb24ca1203e91f033990489b9d2e63d776
KevinCeyland <kevin-78440@hotmail.fr>
2021-02-05 16:33:13 +0100

    Partie actualité quasi fini

--- content/actualite.php
+++ content/actualite.php
@@ -5,9 +5,9 @@
     <h1 class="text-center text-white section-heading text-uppercase text-white mb-5 mt-5">Articles concernant Michael Jackson</h1>
     <div class="row g-2">
         <div class="col-6">
-            <h3 class="ml-5 text-white lead text-uppercase text-white">Article</h3>
+            <h3 class="text-white lead text-uppercase text-white">Michael Jackson : les détails inquiétants de son autopsie</h3>
             <div class="media">
-                <a href=""><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
+                <a href="https://www.rtl.fr/culture/medias-people/michael-jackson-les-details-inquietants-de-son-autopsie-7800944589" target="blank_"><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
             </div>
         </div>
         <div class="col-6">
@@ -17,17 +17,17 @@
         </div>
         <div class="col-6">
             <div class="p-3">
-            <h3 class="ml-5 text-white lead text-uppercase text-white">Article</h3>
+            <h3 class="text-white lead text-uppercase text-white">Michael Jackson incontinent ? Cette raison pour laquelle la star portait des pantalons noirs</h3>
                 <div class="media">
-                    <a href=""><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
+                    <a href="https://www.closermag.fr/people/michael-jackson-incontinent-cette-raison-pour-laquelle-la-star-portait-des-panta-1165805"><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
                 </div>
             </div>
         </div>
         <div class="col-6">
             <div class="p-3">
-            <h1 class="text-white lead text-uppercase text-white">Article</h1>
+            <h1 class="text-white lead text-uppercase text-white">Michael Jackson : les terribles révélations sur ses derniers instants</h1>
                 <div class="media">
-                    <a href=""><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
+                    <a href="https://www.femmeactuelle.fr/actu/news-actu/michael-jackson-les-terribles-revelations-sur-ses-derniers-instants-2098148"><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
                 </div>
             </div>
         </div>
@@ -37,10 +37,10 @@
             <div class="p-3"></div>
         </div>
         <div class="col-6">
-            <div class="p-3">
-            <h3 class="text-white lead text-uppercase text-white">Article</h3>
-                <div class="media">
-                    <a href="https://www.purepeople.com/article/michael-jackson-quand-brooke-shields-refusait-sa-demande-en-mariage_a420305/1">
+            <div class="p-3 ">
+            <h3 class="text-white lead text-uppercase text-white">Michael Jackson : Quand Brooke Shields refusait sa demande en mariage</h3>
+                <div class="media ">
+                    <a target="blank_" href="https://www.purepeople.com/article/michael-jackson-quand-brooke-shields-refusait-sa-demande-en-mariage_a420305/1">
                         <img src="../img/newspaper.png" class="img-fluid newspaper">
                     </a>
                 </div>
