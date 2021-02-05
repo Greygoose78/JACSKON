d7c8ef830161a73e9c6b60f998dc8bf7c1af312b
KevinCeyland <kevin-78440@hotmail.fr>
2021-02-05 16:15:43 +0100

    Modification de la page actualité en cours

--- content/actualite.php
+++ content/actualite.php
@@ -1,3 +1,60 @@
 <?php include "../partials/header.php" ?>
 <?php include "../partials/sidebar.php" ?>
+
+<div class="container p-5">
+    <h1 class="text-center text-white section-heading text-uppercase text-white mb-5 mt-5">Articles concernant Michael Jackson</h1>
+    <div class="row g-2">
+        <div class="col-6">
+            <h3 class="ml-5 text-white lead text-uppercase text-white">Article</h3>
+            <div class="media">
+                <a href=""><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
+            </div>
+        </div>
+        <div class="col-6">
+        </div>
+        <div class="col-6">
+            <div class="p-3"></div>
+        </div>
+        <div class="col-6">
+            <div class="p-3">
+            <h3 class="ml-5 text-white lead text-uppercase text-white">Article</h3>
+                <div class="media">
+                    <a href=""><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="p-3">
+            <h1 class="text-white lead text-uppercase text-white">Article</h1>
+                <div class="media">
+                    <a href=""><img src="../img/newspaper.png" class="img-fluid newspaper"></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+        </div>
+        <div class="col-6">
+            <div class="p-3"></div>
+        </div>
+        <div class="col-6">
+            <div class="p-3">
+            <h3 class="text-white lead text-uppercase text-white">Article</h3>
+                <div class="media">
+                    <a href="https://www.purepeople.com/article/michael-jackson-quand-brooke-shields-refusait-sa-demande-en-mariage_a420305/1">
+                        <img src="../img/newspaper.png" class="img-fluid newspaper">
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+
+
+
+
 <?php include "../partials/footer.php" ?>
