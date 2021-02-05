e5cf842bcb586e5ea8a3ccd2767662b0b54d6828
KevinCeyland <kevin-78440@hotmail.fr>
2021-02-05 15:25:59 +0100

    Page d'accueil + biographie terminé !

--- index.php
+++ index.php
@@ -1,48 +1,99 @@
 <?php include "partials/header.php" ?>
 <?php include "partials/sidebar.php" ?>
-        <!-- Masthead-->
-        <header class="masthead">
-            <div class="container">
-                <!-- <div class="masthead-subheading">Welcome To Our Studio!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a> -->
-            </div>
-        </header>
-        <!-- Services-->
-        <section class="page-section" id="services">
-            <div class="container">
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Services</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-                <div class="row text-center">
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                        </span>
-                        <h4 class="my-3">E-Commerce</h4>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-                        </span>
-                        <h4 class="my-3">Responsive Design</h4>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-                        </span>
-                        <h4 class="my-3">Web Security</h4>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+<!-- Masthead-->
+<header class="masthead">
+
+</header>
+<!-- Services-->
+<section class="page-section" id="services">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase text-white" style="font-size:50px;">MICHAEL JACKSON</h2>
+            <h3 class="section-subheading text-muted">THE KING OF THE POP</h3>
+        </div>
+    </div>
+    <div class="container">
+        <div class="container">
+            <div class="row g-2">
+                <div class="col-6 ">
+                    <div class= "media">
+                        <img src="../img/mj-young.jpg" class="mr-3 mt-3 img-fluid" width="200" alt="Avatar utilisateur">
+                        <div class="media-body">
+                            <h5 class="mt-0"></h5>
+                            <p class="lead text-white">Michael Jackson, né le 29 août 1958 à Gary (Indiana) et mort le 25 juin 2009 à Los Angeles (Californie), est un auteur-compositeur-interprète, danseur-chorégraphe et acteur américain.
+                                Reconnu comme l’artiste le plus titré de tous les temps, il est une figure principale de l'histoire de l'industrie du spectacle et l'une des icônes culturelles majeures du xxe siècle.</p>
+                            <blockquote class="blockquote mt-4 mb-0">
+                                <footer class="blockquote-footer">
+                                    <cite></cite>
+                                </footer>
+                            </blockquote>
+                        </div>
                     </div>
                 </div>
+                <div class="col-6">
+                </div>
+                <div class="col-6">
+                    <div class="p-3"></div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3">
+                        <div class="media">
+                            <div class="media-body">
+                                <h5 class="mt-0"></h5>
+                                <p class="lead text-white">Septième d'une famille de neuf enfants, il chante avec ses frères dès l'âge de six ans et commence une carrière professionnelle à onze ans au sein des Jackson Five, groupe formé avec ses frères aînés. Tout en restant membre du groupe (jusqu'en 1984), il entame en 1971 une carrière solo. Cinq de ses albums solo parus de son vivant figurent parmi les albums les plus vendus au monde : Off the Wall (1979), Thriller (1982), Bad (1987), Dangerous (1991) et HIStory (1995).</p>
+                                <blockquote class="blockquote mt-4 mb-0">
+                                    <footer class="blockquote-footer">
+                                        <cite></cite>
+                                    </footer>
+                                </blockquote>
+                            </div>
+                            <img src="../img/mj-jackson-five.jpg" class="ml-3 mt-3 img-fluid float-right" width="200" alt="Avatar utilisateur">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3">
+                        <div class="media">
+                            <img src="../img/mj-thriller.jpg" class="mr-3 mt-3 img-fluid" width="200" alt="Avatar utilisateur">
+                            <div class="media-body">
+                                <h5 class="mt-0"></h5>
+                                <p class="lead text-white"> Dans les années 1980, il devient une figure majeure de la musique pop. Ses clips musicaux, ambitieux et novateurs, sont réalisés comme des courts métrages, notamment pour les titres Beat It, Billie Jean, Thriller, Bad ou Smooth Criminal. Il popularise de nombreux pas de danse, dont le moonwalk, qui devient sa signature. Ayant fusionné les genres de musique soul, funk, pop et rock, son style vocal et musical continue d'influencer de nombreux artistes de hip-hop, pop et r'n'b contemporain.</p>
+                                <blockquote class="blockquote mt-4 mb-0">
+                                    <footer class="blockquote-footer">
+                                        <cite></cite>
+                                    </footer>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                </div>
+                <div class="col-6">
+                    <div class="p-3"></div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3">
+                        <div class="media">
+                            <img src="../img/mj-blanc.jpg" class="mr-3 mt-3 img-fluid" width="200" alt="Avatar utilisateur">
+                            <div class="media-body">
+                                <h5 class="mt-0"></h5>
+                                <p class="lead text-white">Surnommé « le roi de la pop » (« The King of Pop »), Michael Jackson a battu plusieurs records dans l'industrie du disque. Avec plus de 350 millions de disques vendus dans le monde, il se classe parmi les trois plus gros vendeurs de disques de tous les temps, derrière les Beatles et Elvis Presley.</p>
+                                <blockquote class="blockquote mt-4 mb-0">
+                                    <footer class="blockquote-footer">
+                                        <cite></cite>
+                                    </footer>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </section>
-        <?php include "partials/footer.php" ?>
-    </body>
+        </div>
+    </div>
+</section>
+<?php include "partials/footer.php" ?>
+</body>
+
 </html>
